1089aa119c0fa4d8f3bd64149c175ce40ab299ae
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-21 10:04:55 +0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['spoonacular.com','www.themealdb.com'],
+  },
+};
+module.exports = nextConfig;
